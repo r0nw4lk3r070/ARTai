75ce63fba9905a951b3f388e59f8628933c4d5cf
ronwalker <cryptoron@gmx.com>
2025-03-14 14:48:39 +0100

    Force clear repo for ARTV6, keep only .gitignore

--- ARTcore/interface/control.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-class ControlModule:
-    def __init__(self, parent, art_instance):
-        self.art = art_instance
-        self.frame = tk.Frame(parent, width=400)
-        self.label = tk.Label(self.frame, text="Control Panel - Coming Soon!")
-        self.label.pack(fill=tk.BOTH, expand=True, padx=5, pady=5)
